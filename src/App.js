6a0a6d30fdef4147bbfaeb21aeb28f96da9405db
Thomas Kluth <t.kluth@posteo.de>
2019-01-22 12:32:32 +0100

    output of gp_create()

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
+/**
+ * geoplumber R package code.
+ */
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Switch, Route } from 'react-router-dom';
+
+import Welcome from './Welcome';
+import Header from './components/Header';
+
 import './App.css';
 
+
+/**
+ * Separate the Header and the main content.
+ * Up to this point we are still not using SSR
+ */
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <main>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Welcome} />
+        </Switch>
+      </main>
+    )
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
+/**
+ * geoplumber R package code.
+ */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+// TODO: register/unregister needed?
+import { unregister } from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// *** Do NOT remove, it seems the location for icon is missing or something
+import L from 'leaflet';
+delete L.Icon.Default.prototype._getIconUrl;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
+// ***end
+
+/**
+ * Separating index.js and App.js has the benefit of
+ * doing above like launch configs and keep App.js
+ * clear for React application.
+ */
+ReactDOM.render(
+    <BrowserRouter>
+      <App />
+  </BrowserRouter>, document.getElementById('root'));
+
+unregister();
